e32be4483c6b1afeed082676cb66cb6334561486
Pikulkaew Boonpeng <pkboonpeng@gmail.com>
2023-11-20 11:20:49 -0500

    init

---
+++ src/AnimalShow.js
@@ -0,0 +1,7 @@
+function AnimalShow() {
+    return (
+        <div>Show</div>
+    )
+}
+
+export default AnimalShow;
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>Add Animal</button>
+            <p>
+                Number of Animals: {count}
+            </p>
+        </div>
+    )
+}
+
+export default App;
